6395af414f6f0d244160c3c42d1cadf12dda1e80
Yasmim Neves <yascneves@hotmail.com>
2023-10-23 11:39:00 -0300

    deletado da branch 23/10

--- setembro-2023/exercicio_log-prog_notas.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    float n1, n2, n3, media;
-    printf("digite as tres notas:\n");
-    scanf("%f %f %f", &n1, &n2, &n3);
-    media=(n1+n2+n3)/3;
-    printf("a media e: %3.f", media);
-    return 0;
-}
